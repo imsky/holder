843db0d4416f8b1cf3b8a843c0f8bec8a2427b67
Ivan Malopinsky <ivan@imsky.org>
2012-06-20 13:30:14 -0400

    update selector to always return array

--- holder.js
+++ holder.js
@@ -15,7 +15,12 @@ var Holder = Holder || {};
     function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}};
 		
 	//https://gist.github.com/991057 by Jed Schmidt with modifications
-	function selector(a){a=a.match(/^(\W)?(.*)/);var b=document["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);return b.length?b:[b]}
+	function selector(a){
+		a=a.match(/^(\W)?(.*)/);var b=document["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);
+		var ret=[];
+		b!=null&&(b.length?ret=b:b.length==0?ret=b:ret=[b])
+		return ret;
+	}
 	
 	//object property extend
 	function extend(a,b){var c={};for(var d in a)c[d]=a[d];for(var e in b)c[e]=b[e];return c}
@@ -84,17 +89,17 @@ var Holder = Holder || {};
 		name != null && theme != null && (settings.themes[name] = theme);
 		return app;
 	}
-	app.add_image = function (src, node) {
-		var img = document.createElement("img")
-		img.setAttribute("data-src", src);
-		var _node = selector(node)
-		if(_node) {
-			if(_node.length) {
-				_node = _node[0];
+	app.add_image = function (src, el) {
+		var node = selector(el);
+		console.log(node)
+		if(node.length) {
+			for(var i = 0, l = node.length; i < l; i++){
+				var img = document.createElement("img")
+				img.setAttribute("data-src", src);
+				node[i].appendChild(img);
 			}
-			_node.appendChild(img);
 		}
-		return _node ? app : false;
+		return app;
 	}
 	app.run = function (o) {
 		var options = extend(settings, o),
