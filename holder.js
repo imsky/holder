af17a6da18a95b28e7807a1c4f5c774ab28c5972
Ivan Malopinsky <ivan@imsky.org>
2012-06-20 13:41:26 -0400

    update js

--- holder.js
+++ holder.js
@@ -1,22 +1,37 @@
 /*
+ 
  Holder - client side image placeholders
  (c) 2012 Ivan Malopinsky / http://imsky.co
- Provided under the BSD License. Commercial use requires attribution.
+ 
+ Provided under the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+ Commercial use requires attribution.
+ 
 */
 
 var Holder = Holder || {};
 (function (app, win) {
-	
-    //https://gist.github.com/991057 by Jed Schmidt
-    function selector(a,b){a=a.match(/^(\W)?(.*)/);return(b||document)["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2])};
     
     //http://javascript.nwbox.com/ContentLoaded by Diego Perini with modifications
     function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}};
+		
+	//https://gist.github.com/991057 by Jed Schmidt with modifications
+	function selector(a){
+		a=a.match(/^(\W)?(.*)/);var b=document["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);
+		var ret=[];
+		b!=null&&(b.length?ret=b:b.length==0?ret=b:ret=[b])
+		return ret;
+	}
 	
 	//object property extend
 	function extend(a,b){var c={};for(var d in a)c[d]=a[d];for(var e in b)c[e]=b[e];return c}
 	
 	function draw(canvas, dimensions, template) {
+		
+	if(!canvas.getContext){
+		return false;
+	}
+	
+	var ctx = canvas.getContext("2d");
 		
 	var dimension_arr = [dimensions.height, dimensions.width].sort();
 		
@@ -27,9 +42,7 @@ var Holder = Holder || {};
 		
 	canvas.setAttribute("width", dimensions.width);
 	canvas.setAttribute("height", dimensions.height);
-	
-	var ctx = canvas.getContext("2d");
-	
+
 	ctx.textAlign = "center";
 	ctx.textBaseline = "middle";
 	ctx.fillStyle = template.background;
@@ -76,23 +89,22 @@ var Holder = Holder || {};
 		name != null && theme != null && (settings.themes[name] = theme);
 		return app;
 	}
-	app.add_image = function (src, node) {
-		var img = document.createElement("img")
-		img.setAttribute("data-src", src);
-		var _node = selector(node)
-		if(_node) {
-			if(_node.length) {
-				_node = _node[0];
+	app.add_image = function (src, el) {
+		var node = selector(el);
+		if(node.length) {
+			for(var i = 0, l = node.length; i < l; i++){
+				var img = document.createElement("img")
+				img.setAttribute("data-src", src);
+				node[i].appendChild(img);
 			}
-			_node.appendChild(img);
 		}
-		return _node ? app : false;
+		return app;
 	}
 	app.run = function (o) {
 		var options = extend(settings, o),
-			images = selector(options.images);
-		images = images.length != null ? images : [images];
-		preempted = true;
+			images = selector(options.images),
+			preempted = true;
+			
 		for(var l = images.length, i = 0; i < l; i++) {
 			var dimensions, theme = settings.themes.gray;
 			src = images[i].getAttribute("data-src") || images[i].getAttribute("src");
@@ -119,7 +131,7 @@ var Holder = Holder || {};
 				}
 				if(render){
 					images[i].setAttribute("data-src", src);
-					images[i].setAttribute("src", draw(canvas, dimensions, theme));
+					images[i].setAttribute("src", draw(canvas, dimensions, theme) || src);
 				}
 			}
 		}
