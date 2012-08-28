0309ffb36dfe793ecef3afd4c6ecd8090ad72a2e
Ivan Malopinsky <ivan@imsky.co>
2012-08-27 21:05:16 -0400

    update file

--- holder.js
+++ holder.js
@@ -1,6 +1,6 @@
 /*
 
-Holder - 1.2 - client side image placeholders
+Holder - 1.3 - client side image placeholders
 (c) 2012 Ivan Malopinsky / http://imsky.co
 
 Provided under the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
@@ -10,6 +10,10 @@ Commercial use requires attribution.
 
 var Holder = Holder || {};
 (function (app, win) {
+	
+var preempted = false,
+fallback = false,
+canvas = document.createElement('canvas');
 
 //http://javascript.nwbox.com/ContentLoaded by Diego Perini with modifications
 function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}};
@@ -28,8 +32,8 @@ function draw(ctx, dimensions, template) {
 	var maxFactor = Math.round(dimension_arr[1] / 16),
 		minFactor = Math.round(dimension_arr[0] / 16);
 	var text_height = Math.max(template.size, maxFactor);
-	canvas.setAttribute("width", dimensions.width);
-	canvas.setAttribute("height", dimensions.height);
+	canvas.width = dimensions.width;
+	canvas.height = dimensions.height;
 	ctx.textAlign = "center";
 	ctx.textBaseline = "middle";
 	ctx.fillStyle = template.background;
@@ -44,12 +48,7 @@ function draw(ctx, dimensions, template) {
 	ctx.fillText(text, (dimensions.width / 2), (dimensions.height / 2), dimensions.width);
 	return canvas.toDataURL("image/png");
 }
-var dimensions_regex = /([0-9]+)x([0-9]+)/;
-var hex_regex = /#([0-9a-f]{3,})\:#([0-9a-f]{3,})/i;
-var text_regex = /text\:(.*)/;
-var preempted = false,
-	fallback = false;
-var canvas = document.createElement('canvas');
+
 if (!canvas.getContext) {
 	fallback = true;
 } else {
@@ -83,6 +82,44 @@ var settings = {
 	}
 };
 
+
+
+app.flags = {
+	dimensions: {
+		regex: /([0-9]+)x([0-9]+)/,
+		output: function(val){
+			var exec = this.regex.exec(val);
+			return {
+				width: +exec[1],
+				height: +exec[2]
+			}
+		}
+	},
+	colors: {
+		regex: /#([0-9a-f]{3,})\:#([0-9a-f]{3,})/i,
+		output: function(val){
+			var exec = this.regex.exec(val);
+			return {
+					size: settings.themes.gray.size,
+					foreground: "#" + exec[2],
+					background: "#" + exec[1]
+					}
+		}
+	},
+	text: {
+		regex: /text\:(.*)/,
+		output: function(val){
+			return this.regex.exec(val)[1];
+		}
+	}
+}
+
+for(var flag in app.flags){
+	app.flags[flag].match = function (val){
+		return val.match(this.regex)
+	}
+}
+
 app.add_theme = function (name, theme) {
 	name != null && theme != null && (settings.themes[name] = theme);
 	return app;
@@ -104,48 +141,44 @@ app.run = function (o) {
 	var options = extend(settings, o),
 		images = selector(options.images),
 		preempted = true;
+		
 	for (var l = images.length, i = 0; i < l; i++) {
-		var dimensions, theme = settings.themes.gray;
+		var theme = settings.themes.gray;
 		var src = images[i].getAttribute("data-src") || images[i].getAttribute("src");
 		if ( !! ~src.indexOf(options.domain)) {
-			var render = false;
-			for (var flags = src.substr(src.indexOf(options.domain) + options.domain.length + 1).split("/"), sl = flags.length, j = 0; j < sl; j++) {
-				var exec;
-				if (flags[j].match(dimensions_regex)) {
-					exec = dimensions_regex.exec(flags[j]);
-					dimensions = {
-						width: +exec[1],
-						height: +exec[2]
-					};
+			var render = false,
+				dimensions = null,
+				text = null;
+			var flags = src.substr(src.indexOf(options.domain) + options.domain.length + 1).split("/");
+			for (sl = flags.length, j = 0; j < sl; j++) {
+				if (app.flags.dimensions.match(flags[j])) {
 					render = true;
-				} else if (flags[j].match(hex_regex)) {
-					exec = hex_regex.exec(flags[j]);
-					theme = {
-						size: settings.themes.gray.size,
-						foreground: "#" + exec[2],
-						background: "#" + exec[1]
-					};
-				}
-				else if (options.themes[flags[j]]) {
+					dimensions = app.flags.dimensions.output(flags[j]);
+				} else if (app.flags.colors.match(flags[j])) {
+					theme = app.flags.colors.output(flags[j]);
+				} else if (options.themes[flags[j]]) {
+					//If a theme is specified, it will override custom colors
 					theme = options.themes[flags[j]];
-				}
-				else if(flags[j].match(text_regex)){
-						exec = text_regex.exec(flags[j]);
-						theme.text = exec[1];
+				} else if (app.flags.text.match(flags[j])) {
+					text = app.flags.text.output(flags[j]);
 				}
 			}
 			if (render) {
 				images[i].setAttribute("data-src", src);
-				var dimensions_caption = dimensions.width+"x"+dimensions.height;
-				images[i].setAttribute("alt", theme.text ? theme.text + " ["+dimensions_caption+"]" : dimensions_caption);
+				var dimensions_caption = dimensions.width + "x" + dimensions.height;
+				images[i].setAttribute("alt", text ? text : theme.text ? theme.text + " [" + dimensions_caption + "]" : dimensions_caption);
 				
 				//Fallback
 				images[i].style.width = dimensions.width + "px";
 				images[i].style.height = dimensions.height + "px";
 				images[i].style.backgroundColor = theme.background;
 				
-				if(!fallback){
-				images[i].setAttribute("src", draw(ctx, dimensions, theme));
+				var theme = (text ? extend(theme, {
+						text: text
+					}) : theme);
+				
+				if (!fallback) {
+					images[i].setAttribute("src", draw(ctx, dimensions, theme));
 				}
 			}
 		}
