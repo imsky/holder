9c5dcaf115e50cfe2a229c612fdc6d32c3c2a556
John-Philip Johansson <seriema@gmail.com>
2013-07-10 19:55:15 +0530

    Add JSHint task to Grunt

--- holder.js
+++ holder.js
@@ -19,15 +19,15 @@ canvas = document.createElement('canvas');
 document.getElementsByClassName||(document.getElementsByClassName=function(e){var t=document,n,r,i,s=[];if(t.querySelectorAll)return t.querySelectorAll("."+e);if(t.evaluate){r=".//*[contains(concat(' ', @class, ' '), ' "+e+" ')]",n=t.evaluate(r,t,null,0,null);while(i=n.iterateNext())s.push(i)}else{n=t.getElementsByTagName("*"),r=new RegExp("(^|\\s)"+e+"(\\s|$)");for(i=0;i<n.length;i++)r.test(n[i].className)&&s.push(n[i])}return s})
 
 //getComputedStyle polyfill
-window.getComputedStyle||(window.getComputedStyle=function(e,t){return this.el=e,this.getPropertyValue=function(t){var n=/(\-([a-z]){1})/g;return t=="float"&&(t="styleFloat"),n.test(t)&&(t=t.replace(n,function(){return arguments[2].toUpperCase()})),e.currentStyle[t]?e.currentStyle[t]:null},this})
+window.getComputedStyle||(window.getComputedStyle=function(e){return this.el=e,this.getPropertyValue=function(t){var n=/(\-([a-z]){1})/g;return t=="float"&&(t="styleFloat"),n.test(t)&&(t=t.replace(n,function(){return arguments[2].toUpperCase()})),e.currentStyle[t]?e.currentStyle[t]:null},this})
 
 //http://javascript.nwbox.com/ContentLoaded by Diego Perini with modifications
-function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}};
+function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}}
 
 //https://gist.github.com/991057 by Jed Schmidt with modifications
 function selector(a){
 	a=a.match(/^(\W)?(.*)/);var b=document["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);
-	var ret=[];	b!=null&&(b.length?ret=b:b.length==0?ret=b:ret=[b]);	return ret;
+	var ret=[];	b!==null&&(b.length?ret=b:b.length===0?ret=b:ret=[b]);	return ret;
 }
 
 //shallow object property extend
@@ -35,10 +35,12 @@ function extend(a,b){var c={};for(var d in a)c[d]=a[d];for(var e in b)c[e]=b[e];
 
 //hasOwnProperty polyfill
 if (!Object.prototype.hasOwnProperty)
-	Object.prototype.hasOwnProperty = function(prop) {
+    /*jshint -W001 */
+    Object.prototype.hasOwnProperty = function(prop) {
 		var proto = this.__proto__ || this.constructor.prototype;
 		return (prop in this) && (!(prop in proto) || proto[prop] !== this[prop]);
 	}
+    /*jshint +W001 */
 
 function text_size(width, height, template) {
 	height = parseInt(height,10);
@@ -118,7 +120,7 @@ function render(mode, el, holder, src) {
 		} else {
 			el.style.width = dimensions.width + "px"
 		}
-		if (el.style.display == "inline" || el.style.display == "") {
+		if (el.style.display == "inline" || el.style.display === "") {
 			el.style.display = "block";
 		}
 		if (fallback) {
@@ -129,7 +131,7 @@ function render(mode, el, holder, src) {
 			fluid_update(el);
 		}
 	}
-};
+}
 
 function fluid_update(element) {
 	var images;
@@ -138,7 +140,7 @@ function fluid_update(element) {
 	} else {
 		images = [element]
 	}
-	for (i in images) {
+	for (var i in images) {
 		var el = images[i]
 		if (el.holderData) {
 			var holder = el.holderData;
@@ -181,7 +183,7 @@ function parse_flags(flags, options) {
 
 	return render ? ret : false;
 
-};
+}
 
 
 
@@ -371,8 +373,8 @@ app.run = function (o) {
 	}
 
 	for (l = images.length, i = 0; i < l; i++) {
-	    
-		var attr_src = attr_data_src = src = null;
+	    var attr_data_src, attr_src;
+        attr_src = attr_data_src = src = null;
 		
 		try{
 		    attr_src = images[i].getAttribute("src");
