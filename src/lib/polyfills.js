f17f825485fa4ebff489934961c8549fff2d5a4c
Ivan Malopinsky <ivan@imsky.co>
2015-05-02 21:51:43 -0400

    add performance.now polyfill, fix #134

--- src/lib/polyfills.js
+++ src/lib/polyfills.js
@@ -137,6 +137,43 @@ if (!Object.prototype.hasOwnProperty){
     /*jshint +W001, +W103 */
 }
 
+// @license http://opensource.org/licenses/MIT
+// copyright Paul Irish 2015
+
+
+// Date.now() is supported everywhere except IE8. For IE8 we use the Date.now polyfill
+//   github.com/Financial-Times/polyfill-service/blob/master/polyfills/Date.now/polyfill.js
+// as Safari 6 doesn't have support for NavigationTiming, we use a Date.now() timestamp for relative values
+
+// if you want values similar to what you'd get with real perf.now, place this towards the head of the page
+// but in reality, you're just getting the delta between now() calls, so it's not terribly important where it's placed
+
+
+(function(){
+
+  if ("performance" in window == false) {
+      window.performance = {};
+  }
+  
+  Date.now = (Date.now || function () {  // thanks IE8
+	  return new Date().getTime();
+  });
+
+  if ("now" in window.performance == false){
+    
+    var nowOffset = Date.now();
+    
+    if (performance.timing && performance.timing.navigationStart){
+      nowOffset = performance.timing.navigationStart
+    }
+
+    window.performance.now = function now(){
+      return Date.now() - nowOffset;
+    }
+  }
+
+})();
+
 //requestAnimationFrame polyfill for older Firefox/Chrome versions
 if (!window.requestAnimationFrame) {
   if (window.webkitRequestAnimationFrame) {
