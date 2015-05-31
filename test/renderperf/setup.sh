f991472e6bdfb69be69bab9d302bc514b430e124
Ivan Malopinsky <ivan@imsky.co>
2015-05-31 18:28:16 -0400

    add historical render performance benchmarks

---
+++ test/renderperf/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+RELEASES=(v1.0 v1.1 v1.2 v1.3 v1.4 v1.5 v1.6 v1.7 v1.8 v1.9 1.9.0 v2.0 v2.1.0 v2.2.0 v2.3.0 v2.3.2 v2.4.0 v2.5.0 v2.5.2 v2.6.0 v2.7.0 v2.7.1)
+
+mkdir -p releases
+
+for release in ${RELEASES[*]}
+do
+    echo "Downloading Holder $release"
+    wget -qO- https://github.com/imsky/holder/archive/$release.tar.gz | tar xz -C releases/
+done
